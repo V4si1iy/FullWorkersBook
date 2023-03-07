a02fce642842865992e8e141deefe68c7d0b4b78
Vasiliy <pilaroiding@gmail.com>
2023-03-08 01:00:39 +0300

    Initial commit

---
+++ src/main/java/home/work/fullworkersbook/FullWorkersBookApplication.java
@@ -0,0 +1,13 @@
+package home.work.fullworkersbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullWorkersBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullWorkersBookApplication.class, args);
+    }
+
+}
